e485de85fb30615f3dbeed3fe2700f599f5216bd
DAN0N123 <filipskubis01@wp.pl>
2024-11-17 15:07:48 +0100

    .

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -53,116 +53,118 @@ export default function ProductModal({
       window.removeEventListener("click", closeFunction);
     };
   }, [setProductModal]);
-
-  return (
-    <div className="absolute flex inset-0 justify-center pt-[30%] w-screen h-screen">
-      <div className="fixed w-[9999px] h-[9999px] top-0 left-0 backdrop-blur-sm z-[9998]"></div>
-      <form
-        className="relative w-[80vw] h-[35vh] bg-white shadow-xl border-[1px] border-darkcoral rounded-lg z-[9999] p-4 pt-8 flex flex-col gap-4"
-        onSubmit={handleAddProduct}
-        ref={modalRef}
-      >
-        <button
-          className="absolute right-2 top-2"
-          onClick={() => {
-            setProductModal(false);
-          }}
+  if (data) {
+    return (
+      <div className="absolute flex inset-0 justify-center pt-[30%] w-screen h-screen">
+        <div className="fixed w-[9999px] h-[9999px] top-0 left-0 backdrop-blur-sm z-[9998]"></div>
+        <form
+          className="relative w-[80vw] h-[35vh] bg-white shadow-xl border-[1px] border-darkcoral rounded-lg z-[9999] p-4 pt-8 flex flex-col gap-4"
+          onSubmit={handleAddProduct}
+          ref={modalRef}
         >
-          <X />
-        </button>
-        <div className="flex flex-col gap-2 items-center">
-          <label htmlFor="productSelect" className="text-lg">
-            Produkt:
-          </label>
-          <select
-            name="productSelect"
-            id="productSelect"
-            onChange={handleChange}
-            required
-            className="w-full p-2 border-[1px] border-[#CCCCCC]"
+          <button
+            className="absolute right-2 top-2"
+            onClick={() => {
+              setProductModal(false);
+            }}
           >
-            <option value=""> - Wybierz z listy -</option>
-            <optgroup label="Priorytetowe produkty" className="text-coral">
-              {prioritizedProducts.map((name, index) => (
-                <option value={name} key={`priority-${index}`}>
-                  {name}
-                </option>
-              ))}
-            </optgroup>
-            <optgroup label="Stała oferta" className="text-coral">
-              {data
-                .filter(
-                  (product) =>
-                    !product.seasonal &&
-                    !prioritizedProducts.includes(product.name)
-                )
-                .map(({ name }, index) => (
-                  <option value={name} key={`regular-${index}`}>
-                    {name}
-                  </option>
-                ))}
-            </optgroup>
-            <optgroup label="Sezonowe" className="text-coral">
-              {data
-                .filter(
-                  (product) =>
-                    product.seasonal &&
-                    !prioritizedProducts.includes(product.name)
-                )
-                .map(({ name }, index) => (
-                  <option value={name} key={`seasonal-${index}`}>
-                    {name}
-                  </option>
-                ))}
-            </optgroup>
-          </select>
-        </div>
-        <div className="flex flex-col gap-2 items-center">
-          <label htmlFor="quantity" className="text-lg">
-            Ilość: {currentProduct ? `(${currentProduct.packagingMethod})` : ""}
-          </label>
-          <div className="flex gap-2">
-            <input
-              type="number"
-              id="quantity"
-              name="quantity"
-              value={quantity}
-              onChange={(e) => {
-                setQuantity(e.target.value);
-              }}
-              defaultValue={1}
-              step="0.01"
+            <X />
+          </button>
+          <div className="flex flex-col gap-2 items-center">
+            <label htmlFor="productSelect" className="text-lg">
+              Produkt:
+            </label>
+            <select
+              name="productSelect"
+              id="productSelect"
+              onChange={handleChange}
               required
-              className="w-[100px] border-[1px] border-[#CCCCCC] p-1 text-lg"
-            />
-            <div className="flex gap-2 h-full justify-center self-start">
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  setQuantity((current) => current + 1);
+              className="w-full p-2 border-[1px] border-[#CCCCCC]"
+            >
+              <option value=""> - Wybierz z listy -</option>
+              <optgroup label="Priorytetowe produkty" className="text-coral">
+                {prioritizedProducts.map((name, index) => (
+                  <option value={name} key={`priority-${index}`}>
+                    {name}
+                  </option>
+                ))}
+              </optgroup>
+              <optgroup label="Stała oferta" className="text-coral">
+                {data
+                  .filter(
+                    (product) =>
+                      !product.seasonal &&
+                      !prioritizedProducts.includes(product.name)
+                  )
+                  .map(({ name }, index) => (
+                    <option value={name} key={`regular-${index}`}>
+                      {name}
+                    </option>
+                  ))}
+              </optgroup>
+              <optgroup label="Sezonowe" className="text-coral">
+                {data
+                  .filter(
+                    (product) =>
+                      product.seasonal &&
+                      !prioritizedProducts.includes(product.name)
+                  )
+                  .map(({ name }, index) => (
+                    <option value={name} key={`seasonal-${index}`}>
+                      {name}
+                    </option>
+                  ))}
+              </optgroup>
+            </select>
+          </div>
+          <div className="flex flex-col gap-2 items-center">
+            <label htmlFor="quantity" className="text-lg">
+              Ilość:{" "}
+              {currentProduct ? `(${currentProduct.packagingMethod})` : ""}
+            </label>
+            <div className="flex gap-2">
+              <input
+                type="number"
+                id="quantity"
+                name="quantity"
+                value={quantity}
+                onChange={(e) => {
+                  setQuantity(e.target.value);
                 }}
-              >
-                <CirclePlus className="w-[2rem] h-auto" />
-              </button>
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  if (quantity - 1 >= 0) {
-                    setQuantity((current) => current - 1);
-                  }
-                }}
-              >
-                <CircleMinus className="w-[2rem] h-auto" />
-              </button>
+                defaultValue={1}
+                step="0.01"
+                required
+                className="w-[100px] border-[1px] border-[#CCCCCC] p-1 text-lg"
+              />
+              <div className="flex gap-2 h-full justify-center self-start">
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setQuantity((current) => current + 1);
+                  }}
+                >
+                  <CirclePlus className="w-[2rem] h-auto" />
+                </button>
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    if (quantity - 1 >= 0) {
+                      setQuantity((current) => current - 1);
+                    }
+                  }}
+                >
+                  <CircleMinus className="w-[2rem] h-auto" />
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="absolute bottom-0 left-0 w-full">
-          <button className="w-full flex justify-center items-center h-[50px] bg-[#f28a7280]">
-            Dodaj
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+          <div className="absolute bottom-0 left-0 w-full">
+            <button className="w-full flex justify-center items-center h-[50px] bg-[#f28a7280]">
+              Dodaj
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
