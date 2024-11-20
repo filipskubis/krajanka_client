0e9f74ff649599eb987d265c547accd5a208c5ef
DAN0N123 <filipskubis01@wp.pl>
2024-11-20 17:33:28 +0100

    .

--- src/components/OrderDetails.jsx
+++ src/components/OrderDetails.jsx
@@ -315,15 +315,6 @@ export default function OrderDetails() {
                 />
                 <p> {data.date || "- ~ -"} </p>
               </div>
-              <div className="flex gap-2 items-center">
-                <Clock
-                  color="#f28a72"
-                  width={"30px"}
-                  height={"auto"}
-                  className="tablet:w-[2rem]  print:w-[1.5rem]"
-                />
-                <p> {data.time || "- ~ -"} </p>
-              </div>
               {data.products.length > 0 && (
                 <div className="gap-2 p-1 grid grid-cols-[minmax(90px,_1.5fr)_1fr_2fr_1fr] text-left w-full print:grid-cols-[1fr_1fr_1fr_1fr] print:gap-1 print:p-0">
                   <p>Nazwa:</p>
