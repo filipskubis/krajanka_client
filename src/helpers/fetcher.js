0e7eb20cda876037ca2f03515de81bad8e502d84
Filip Skubiś <144487707+filipskubis@users.noreply.github.com>
2026-04-10 23:25:03 +0200

    Update fetcher URL to production endpoint

--- src/helpers/fetcher.js
+++ src/helpers/fetcher.js
@@ -1,8 +1,8 @@
-// https://krajankaservertesting-production.up.railway.app${ENDPOINT}
+// https://krajanka.up.railway.app${ENDPOINT}
 // http://localhost:3000${ENDPOINT}
 export default async function fetcher(ENDPOINT, method = "GET", body = null) {
   const response = await fetch(
-    `https://krajankaservertesting-production.up.railway.app${ENDPOINT}`,
+    `https://krajanka.up.railway.app${ENDPOINT}`,
     {
       method,
       headers: {
