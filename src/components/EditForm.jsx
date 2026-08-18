6378441ecce50c96753a55b3a14667bef30c3923
DAN0N123 <filipskubis01@wp.pl>
2026-08-18 14:05:28 +0200

    Allow tenth-unit quantities in internal orders

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -84,7 +84,7 @@ export default function EditForm({ order, close }) {
     console.log("yo");
     const newProducts = products.map((product) => {
       if (product?.id === id) {
-        product.quantity += 0.5;
+        product.quantity = Number((product.quantity + 0.1).toFixed(1));
       }
       return product;
     });
@@ -99,12 +99,12 @@ export default function EditForm({ order, close }) {
 
     if (!productToSubtract) return;
 
-    if (productToSubtract.quantity - 0.5 < 0.5) {
+    if (productToSubtract.quantity - 0.1 < 0.1) {
       removeProduct(id);
     } else {
       const newProducts = products.map((product) => {
         if (product.id === id) {
-          return { ...product, quantity: product.quantity - 0.5 };
+          return { ...product, quantity: Number((product.quantity - 0.1).toFixed(1)) };
         }
         return product;
       });
--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -90,7 +90,7 @@ export default function OrderForm() {
   function handleAdd(id) {
     const newProducts = products.map((product) => {
       if (product.id === id) {
-        product.quantity += 0.5;
+        product.quantity = Number((product.quantity + 0.1).toFixed(1));
       }
       return product;
     });
@@ -105,12 +105,12 @@ export default function OrderForm() {
 
     if (!productToSubtract) return;
 
-    if (productToSubtract.quantity - 0.5 < 0.5) {
+    if (productToSubtract.quantity - 0.1 < 0.1) {
       removeProduct(id);
     } else {
       const newProducts = products.map((product) => {
         if (product.id === id) {
-          return { ...product, quantity: product.quantity - 0.5 };
+          return { ...product, quantity: Number((product.quantity - 0.1).toFixed(1)) };
         }
         return product;
       });
